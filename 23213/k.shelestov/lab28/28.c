d504e64737bf57ceb7390ca34e742d508c6ff66d
Kirill <k.shelestov@g.nsu.ru>
2024-09-26 13:59:16 +0700

    lab28

--- 23213/k.shelestov/lab28/28.c
+++ 23213/k.shelestov/lab28/28.c
@@ -1,33 +1,46 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <time.h>
-#include <libgen.h>
+
 #define NUM_COUNT 100
+
+extern int p2open(const char *, FILE *[2]); 
+extern int p2close(FILE *[2]);              
+
 int main() {
-    FILE *fp;
+    FILE *sort_fp[2]; 
+    FILE *fp[2];     
     int i;
+
     srand(time(NULL));
-    FILE *sort_fp;
-    if ((sort_fp = p2open("sort", "w")) == NULL) {
+
+
+    if (p2open("sort", sort_fp) == -1) {
         perror("Error with open sort");
         return 1;
     }
+
     for (i = 0; i < NUM_COUNT; i++) {
-        fprintf(sort_fp, "%d\n", rand() % 100); 
+        fprintf(sort_fp[1], "%d\n", rand() % 100); 
     }
-    p2close(sort_fp);
-    if ((fp = p2open("sort -n", "r")) == NULL) { 
+    p2close(sort_fp); 
+
+
+    if (p2open("sort -n", fp) == -1) { 
         perror("Error with open sort for reading");
         return 1; 
     }
+
     char line[10];
     i = 0;
-    while (fgets(line, sizeof(line), fp) != NULL) { 
+    while (fgets(line, sizeof(line), fp[0]) != NULL) { 
         printf("%s", line);
         if (++i % 10 == 0) {
             printf("\n"); 
         }
     } 
-    p2close(fp);
+
+    p2close(fp); 
     return 0;
-} 
+}
+
