61d13feaeb1eb956e83f3c769b67dc735f98bbc6
Kirill <k.shelestov@g.nsu.ru>
2024-09-27 13:00:42 +0700

    lab28

--- 23213/k.shelestov/lab28/28.c
+++ 23213/k.shelestov/lab28/28.c
@@ -21,25 +21,28 @@ int main() {
         return 1;
     }
 
+
     for (i = 0; i < NUM_COUNT; i++) {
         fprintf(sort_fp[1], "%d\n", rand() % 100); 
     }
     p2close(sort_fp); 
 
 
-    if (p2open("sort -n", fp) == -1) { 
+    if (p2open("sort", fp) == -1) { 
         perror("Error with open sort for reading");
         return 1; 
     }
 
     char line[10];
     i = 0;
+
     while (fgets(line, sizeof(line), fp[0]) != NULL) { 
         printf("%s", line);
         if (++i % 10 == 0) {
             printf("\n"); 
         }
     } 
+
 
     p2close(fp); 
     return 0;
