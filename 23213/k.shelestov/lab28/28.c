2f88fc70d9f7d9f234d54846b8ba45e8e2282adc
Kirill <k.shelestov@g.nsu.ru>
2024-09-27 13:47:07 +0700

    lab28

--- 23213/k.shelestov/lab28/28.c
+++ 23213/k.shelestov/lab28/28.c
@@ -1,6 +1,7 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <time.h>
+#include <libgen.h>
 
 #define NUM_COUNT 100
 
@@ -14,24 +15,24 @@ int main() {
 
     srand(time(NULL));
 
-
+    // Открываем процесс сортировки для записи
     if (p2open("/usr/bin/sort -n", sort_fp) == -1) {
         perror("Error opening sort for writing");
         return 1;
     }
 
-
+    // Генерируем случайные числа и записываем их в сортируемый процесс
     for (i = 0; i < NUM_COUNT; i++) {
         fprintf(sort_fp[1], "%d\n", rand() % 100); 
     }
-    
 
+    // Закрываем поток записи
     if (p2close(sort_fp) == -1) {
         perror("Error closing sort after writing");
         return 1;
     }
 
-
+    // Открываем процесс сортировки для чтения
     if (p2open("/usr/bin/sort -n", fp) == -1) { 
         perror("Error opening sort for reading");
         return 1; 
@@ -40,7 +41,7 @@ int main() {
     char line[10];
     i = 0;
 
-
+    // Читаем отсортированные числа и выводим их на экран
     while (fgets(line, sizeof(line), fp[0]) != NULL) { 
         printf("%s", line);
         fflush(stdout);
