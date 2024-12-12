bf190c083f48a269d2fbebaf7c1e862274d4c357
Kirill <k.shelestov@g.nsu.ru>
2024-12-12 23:56:58 +0700

    test

---
+++ 23213/k.shelestov/testUID/_posix2_uid_max.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+#include <unistd.h>
+#include <limits.h>
+#include <sys/param.h>
+
+int main() {
+    long max_uid = sysconf(_POSIX2_UID_MAX);
+
+    if (max_uid == -1) {
+        perror("sysconf");
+        return 1;
+    }
+
+    printf("MAX UID: %ld\n", max_uid);
+    return 0;
+}
+
---
+++ 23213/k.shelestov/testUID/maxuid.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+#include <unistd.h>
+#include <sys/param.h>
+
+int main() {
+    long max_uid = sysconf(_SC_MAXUID);
+
+    if (max_uid == -1) {
+        perror("sysconf");
+        return 1;
+    }
+
+    printf("MAX UID: %ld\n", max_uid);
+    return 0;
+}
+
